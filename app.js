a1ae1e2abcd327a8d9c5a4cd5fb32c5106da6649
hehe <emulephpwyj@gmail.com>
2017-08-27 10:36:27 +0800

    update

--- app.js
+++ app.js
@@ -1,64 +1,80 @@
-var express = require("express");
-var path = require("path");
-var favicon = require("serve-favicon");
-var logger = require("morgan");
-var cookieParser = require("cookie-parser");
-var bodyParser = require("body-parser");
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-var routes = require("./routes/index");
-var users = require("./routes/users");
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/", routes);
-app.use("/users", users);
+app.use('/', routes);
+app.use('/users', users);
 
-const { blowfishEncrypt, findUser, createUser } = require("./db");
-const models = require("./models");
+const { blowfishEncrypt, findUser, createUser } = require('./db');
+const models = require('./models');
 
-app.post("/register", (req, res, next) => {
-  createUser(req.body.nickname, req.body.password).spread((user, created) => {
+app.post('/register', (req, res, next) => {
+  let { nickname, password } = req.body;
+  if (!nickname || !password) {
+    res.json({
+      iRet: -1,
+      msg: '参数错误'
+    });
+  }
+
+  createUser(nickname, password).spread((user, created) => {
     if (created) {
       res.json({
         iRet: 0,
-        msg: "注册成功"
+        msg: '注册成功'
       });
     } else {
       res.json({
         iRet: -1,
-        msg: "用户名已存在"
+        msg: '用户名已存在'
       });
     }
   });
 });
 
-app.post("/login", (req, res, next) => {
+app.post('/login', (req, res, next) => {
+  let { nickname, password } = req.body;
+  if (!nickname || !password) {
+    res.json({
+      iRet: -1,
+      msg: '参数错误'
+    });
+  }
+
   findUser(req.body.nickname, req.body.password).then(user => {
     if (!user) {
       res.json({
         iRet: -1,
-        msg: "用户名或密码错误"
+        msg: '用户名或密码错误'
       });
     } else {
       res.json({
         iRet: 0,
         token: blowfishEncrypt(
           JSON.stringify([user.nickname, user.password]),
-          "shaojun"
+          'shaojun'
         ),
-        msg: "登录成功"
+        msg: '登录成功'
       });
     }
   });
@@ -66,7 +82,7 @@ app.post("/login", (req, res, next) => {
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error("Not Found");
+  var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
@@ -75,10 +91,10 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get("env") === "development") {
+if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render("error", {
+    res.render('error', {
       message: err.message,
       error: err
     });
@@ -89,7 +105,7 @@ if (app.get("env") === "development") {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render("error", {
+  res.render('error', {
     message: err.message,
     error: {}
   });
