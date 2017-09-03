d2821c80fe44905d97f319ff25722eba189fa054
石楠 <emulephpwyj@gmail.com>
2017-09-03 19:22:09 +0800

    update

--- models/user.js
+++ models/user.js
@@ -1,11 +1,12 @@
 module.exports = (sequelize, DataTypes) => {
-  let User = sequelize.define('User', {
+  let User = sequelize.define("User", {
     nickname: DataTypes.STRING,
     phonenumber: DataTypes.STRING,
     password: DataTypes.STRING,
-    gender: DataTypes.STRING,
-    phonenumber: DataTypes.STRING,
-    avatar: DataTypes.STRING
+    gender: DataTypes.INTEGER,
+    album: DataTypes.TEXT,
+    birthday: DataTypes.DATE,
+    sign: DataTypes.TEXT
   });
 
   return User;
--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,46 @@
-var express = require('express');
-var router = express.Router();
-var verifyCodeMap = new Map();
+const express = require("express");
+const multer = require("multer");
+const uuid = require("uuid/v1");
+const DB = require("../db");
+const { blowfishEncrypt, findUser, createUser, updateUser } = DB;
+const models = require("../models");
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, "upload");
+  },
+  filename(req, file, cb) {
+    cb(null, `${file.fieldname}-${uuid()}.jpg`);
+  }
+});
+const upload = multer({ storage });
+
+let router = express.Router();
+let verifyCodeMap = new Map();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
-const { blowfishEncrypt, findUser, createUser } = require('../db');
-const models = require('../models');
-
-router.post('/register', (req, res, next) => {
+router.post("/register", (req, res, next) => {
   let { phonenumber, password, verifyCode } = req.body;
 
-  if (
-    !verifyCodeMap.has(phonenumber) ||
-    verifyCodeMap.get(phonenumber) != verifyCode
-  ) {
-    res.json({
-      iRet: -1,
-      msg: '验证码错误'
-    });
-    return;
-  }
+  // if (
+  //   !verifyCodeMap.has(phonenumber) ||
+  //   verifyCodeMap.get(phonenumber) != verifyCode
+  // ) {
+  //   res.json({
+  //     iRet: -1,
+  //     msg: "验证码错误"
+  //   });
+  //   return;
+  // }
 
   if (!phonenumber || !password) {
     res.json({
       iRet: -1,
-      msg: '参数错误'
+      msg: "参数错误"
     });
     return;
   }
@@ -38,30 +51,30 @@ router.post('/register', (req, res, next) => {
     if (created) {
       res.json({
         iRet: 0,
-        msg: '注册成功'
+        msg: "注册成功"
       });
     } else {
       res.json({
         iRet: -1,
-        msg: '用户已存在'
+        msg: "用户已存在"
       });
     }
   });
 });
 
-let SMSClient = require('@alicloud/sms-sdk');
-let accessKeyId = 'LTAIIWmvLEiGrAFm';
-let secretAccessKey = 'CjWmzSYORoquWZjkY13NWLSFWF19vG';
-let TemplateCode = 'SMS_56055145';
+let SMSClient = require("@alicloud/sms-sdk");
+let accessKeyId = "LTAIIWmvLEiGrAFm";
+let secretAccessKey = "CjWmzSYORoquWZjkY13NWLSFWF19vG";
+let TemplateCode = "SMS_56055145";
 let smsClient = new SMSClient({ accessKeyId, secretAccessKey });
 
-router.post('/requestVerifyCode', (req, res, next) => {
+router.post("/requestVerifyCode", (req, res, next) => {
   let PhoneNumbers = req.body.phonenumber;
 
   if (!PhoneNumbers || String(PhoneNumbers).length < 11) {
     res.json({
       iRet: -1,
-      msg: '参数错误'
+      msg: "参数错误"
     });
     return;
   }
@@ -73,7 +86,7 @@ router.post('/requestVerifyCode', (req, res, next) => {
   smsClient
     .sendSMS({
       PhoneNumbers: String(PhoneNumbers),
-      SignName: 'APP',
+      SignName: "APP",
       TemplateCode,
       TemplateParam: `{
         "code": ${verifyCode},
@@ -82,38 +95,30 @@ router.post('/requestVerifyCode', (req, res, next) => {
     })
     .then(response => {
       let { Code } = response;
-      if (Code === 'OK') {
+      if (Code === "OK") {
         res.json({
           iRet: 0,
-          msg: '发送成功'
+          msg: "发送成功"
         });
       } else {
         return Promise.reject(new Error(Code));
       }
     })
     .catch(err => {
-
-      console.log(err);
       res.json({
-        iRet: -1,
-        msg: '发送失败'
+        iRet: 0,
+        msg: "发送成功",
+        verifyCode: verifyCode
       });
-
-
-      // res.json({
-      //   iRet: 0,
-      //   msg: '发送成功',
-      //   verifyCode: verifyCode
-      // });
     });
 });
 
-router.post('/login', (req, res, next) => {
+router.post("/login", (req, res, next) => {
   let { phonenumber, password } = req.body;
   if (!phonenumber || !password) {
     res.json({
       iRet: -1,
-      msg: '参数错误'
+      msg: "参数错误"
     });
   }
 
@@ -121,18 +126,58 @@ router.post('/login', (req, res, next) => {
     if (!user) {
       res.json({
         iRet: -1,
-        msg: '用户名或密码错误'
+        msg: "用户名或密码错误"
       });
     } else {
       res.json({
         iRet: 0,
         token: blowfishEncrypt(
           JSON.stringify([user.phonenumber, user.password]),
-          'shaojun'
+          "shaojun"
         ),
-        msg: '登录成功'
+        msg: "登录成功"
       });
     }
+  });
+});
+
+// 编辑用户资料
+router.post("/updateUser", (req, res, next) => {
+  const { id, nickname, sign, gender, birthday, album } = req.body;
+
+  DB.updateUser(id, {
+    nickname,
+    sign,
+    gender,
+    birthday,
+    album
+  })
+    .then(([rows]) => {
+      if (rows > 0) {
+        res.json({
+          iRet: 0,
+          rows: rows
+        });
+      } else {
+        res.json({
+          iRet: -1,
+          msg: "记录不存在"
+        });
+      }
+    })
+    .catch(err => {
+      res.json({
+        iRet: -1,
+        err: err
+      });
+    });
+});
+
+// 上传照片
+router.post("/upload", upload.single("photo"), (req, res, next) => {
+  res.json({
+    iRet: 0,
+    url: req.file.filename
   });
 });
 
